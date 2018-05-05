9bf2f4ba4ae70f5b4ea5115d7f5144b4e8829697
Rasmus Koit <rasmuskoit@gmail.com>
2018-05-05 22:45:03 +0300

    Simple weather app made with React

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h2 className="title-container__subtitle">Find out more about weather and more...</h2>
+    </div>
+);
+
+export default Titles;
